2199266956faa08d6f80a9a641738abb4c431ad3
Jessica Sousa <jessica.persou@gmail.com>
2024-11-05 14:54:11 -0300

    adjustment in the interest calculation method

--- src/main/java/com/postech/lending/creditanalysis/service/util/InterestRateCalculator.java
+++ src/main/java/com/postech/lending/creditanalysis/service/util/InterestRateCalculator.java
@@ -21,24 +21,19 @@ public class InterestRateCalculator {
     public BigDecimal amountToBePaidAfterPeriod(AnalysisCredit analysisCredit) {
         BigDecimal desiredValue = analysisCredit.getRequestedValue();
         int numberInstallment = analysisCredit.getNumberInstallment();
-        BigDecimal monthlyIncome = calculateByMonthlyIncome(analysisCredit.getMonthlyIncome(), ANNUAL_INTEREST_RATE);
 
-        log.info("Iniciando o cálculo do valor a ser pago para o cliente com documento: "  + analysisCredit.getDocument());
+        BigDecimal adjustedAnnualRate = calculateByMonthlyIncome(analysisCredit.getMonthlyIncome(), ANNUAL_INTEREST_RATE);
+        double annualRate = adjustedAnnualRate.doubleValue();
+        double monthlyRateDouble = Math.pow(1 + annualRate, 1.0 / 12.0) - 1;
+        BigDecimal monthlyInterestRate = BigDecimal.valueOf(monthlyRateDouble).setScale(10, RoundingMode.HALF_UP);
 
-        BigDecimal addInterestRate = BigDecimal.ONE.add(monthlyIncome).pow(numberInstallment);
+        log.info("Iniciando o cálculo do valor a ser pago para o cliente com documento: " + analysisCredit.getDocument());
 
-        BigDecimal finalValue = getFinalValue(numberInstallment, desiredValue.multiply(addInterestRate).setScale(2, RoundingMode.HALF_UP));
+        BigDecimal finalValue = desiredValue.multiply(BigDecimal.ONE.add(monthlyInterestRate).pow(numberInstallment)).setScale(2, RoundingMode.HALF_UP);
 
-        log.info(format("Cálculo finalizado com sucesso. Valor final: %s{0}", finalValue));
+        log.info(format("Cálculo finalizado com sucesso. Valor final: {0}", finalValue));
 
         return finalValue;
-    }
-
-    public BigDecimal getFinalValue(int numberInstallment, BigDecimal desiredValue) {
-        CalculationStrategy strategy = CalculationFactory.getCalculationStrategy(numberInstallment);
-        BigDecimal value = strategy.calculate(desiredValue, numberInstallment);
-        log.info("Cálculo para {} anos de juros aplicado.", numberInstallment / 12);
-        return value;
     }
 
     public BigDecimal calculateTotalRate(AnalysisCredit analysisCredit) {
@@ -48,8 +43,12 @@ public class InterestRateCalculator {
     }
 
     public BigDecimal calculateTotalPercentage(AnalysisCredit analysisCredit) {
+        BigDecimal desiredValue = analysisCredit.getRequestedValue();
         BigDecimal totalInterest = amountToBePaidAfterPeriod(analysisCredit);
-        return totalInterest.divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP);
+
+        return totalInterest.subtract(desiredValue)
+                .multiply(BigDecimal.valueOf(100))
+                .divide(desiredValue, 2, RoundingMode.HALF_UP);
     }
 
     public BigDecimal calculateByMonthlyIncome(BigDecimal monthlyIncome, BigDecimal baseInterestRate) {
@@ -64,7 +63,6 @@ public class InterestRateCalculator {
         }
         return baseInterestRate.add(interestRateAdjustment);
     }
-
 }
 
 
